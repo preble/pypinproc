21f699985c97aeaf7c43c6fd1f4fd56a147198c7
Adam Preble <adam@beaker.local>
2009-07-20 22:51:56 -0400

    Made changes to setup for pyprocgame to (hopefully) build better on non-Darwin platforms.

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 # From: http://superjared.com/entry/anatomy-python-c-module/
 from distutils.core import setup, Extension
+import os
+
+uname = os.uname()
+
+extra_compile_args = ['-O0', '-g']
+extra_link_args = []
+if uname[0] == 'Darwin': # Assuming that Darwin is our only platform with i386 and ppc binaries, we'll force this to build for the current platform:
+	extra_compile_args += ['-arch', uname[4]]
+	extra_link_args += ['-arch', uname[4]]
 
 module1 = Extension("pinproc",
 					include_dirs = ['../../include'],
 					libraries = ['usb', 'ftdi', 'pinproc'],
 					library_dirs = ['/usr/local/lib', '../../bin'],
-					extra_compile_args = ['-arch', 'i386', '-O0', '-g'], # Note: Force i386... would prefer to not build universal, just current platform...
-					extra_link_args = ['-arch', 'i386'],
+					extra_compile_args = extra_compile_args,
+					extra_link_args = extra_link_args,
 					sources = ['pypinproc.cpp', 'dmdutil.cpp'])
 
 setup(name = "pinproc",
