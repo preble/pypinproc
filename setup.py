2034913b0a19f7b65134dd531f86aa21d8e3526e
Gerry Stellenberg <gerrys@gerrys-hex.(none)>
2012-02-19 10:25:25 -0600

    Changed version to 1.1

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ module1 = Extension("pinproc",
 					sources = ['pypinproc.cpp', 'dmdutil.cpp', 'dmd.c'])
 
 setup(name = "pinproc",
-      version = "1.00",
+      version = "1.1",
       ext_modules = [module1])
