5cba1aae9f1a1c5cfd536aa28350129dd8880338
Gerry Stellenberg <gerrys@gerrys-hex.(none)>
2013-08-25 21:05:07 -0500

    require ftdi1 library

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if 'ARCH' in os.environ:
 
 module1 = Extension("pinproc",
 					include_dirs = ['../libpinproc/include'],
-					libraries = ['usb', 'ftdi', 'pinproc'],
+					libraries = ['usb', 'ftdi1', 'pinproc'],
 					library_dirs = ['/usr/local/lib', '../libpinproc/bin'],
 					extra_compile_args = extra_compile_args,
 					extra_link_args = extra_link_args,
